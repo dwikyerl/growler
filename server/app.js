f6261612e4458f68a7d172f73d12ec47a027a206
Dwiky Erlangga <dwikyerl@gmail.com>
2018-06-27 08:32:28 +0700

    Add error handlers in server

--- server/app.js
+++ server/app.js
@@ -7,6 +7,9 @@ const mongoose = require('mongoose');
 // Import routes
 const routes = require('./routes');
 
+// Import error handlers
+const errorHandlers = require('./handlers/errorHandlers');
+
 // Initiate App
 const app = express();
 
@@ -27,6 +30,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api', routes);
+
+app.use(errorHandlers.notFound);
+app.use(errorHandlers.validationErrors);
+if (process.env.NODE_ENV === 'development') {
+  app.use(errorHandlers.developmentErrors);
+} else {
+  app.use(errorHandlers.productionErrors);
+}
 
 const server = app.listen(process.env.PORT || 5000, () => {
   console.log(`Server is running on port --> ${server.address().port}`);
